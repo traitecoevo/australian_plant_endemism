30f04174d2943dff46f9f9cd736f683517cca728
stephwoolley <106574502+stephwoolley@users.noreply.github.com>
2022-10-25 18:11:00 +1100

    cleaned up code
     made joining more cohesive and removed smaller group treemaps

--- R/growth_form.R
+++ R/growth_form.R
@@ -1,87 +1,39 @@
-
 #package installation 
 #remotes::install_github("traitecoevo/austraits", build_vignettes = FALSE)
+#install.packages("dplyr")
 #install.packages("treemap")
+#install.packages("RColorBrewer")
 library(austraits) 
 library(dplyr)
 library(treemap)
-library("RColorBrewer")
+library(RColorBrewer)
 
 #austraits growth form data set 
 austraits <- load_austraits(version = "3.0.2", path = "intermediate_data/austraits")
 
 #create new data frame with just the growth form values 
-growth_form <- filter(austraits$traits, trait_name == "plant_growth_form") 
+growth_form <- filter(austraits$traits, trait_name == "plant_growth_form")
 
-#select only necessary info
-growth_form_subset <- select(growth_form, taxon_name, value)
-
-#select only first one of each canonical name 
-growth_form <- growth_form_subset %>%
-  distinct(taxon_name, .keep_all = TRUE)
-
-#Endemic and non-endemic treemap process 
-#load endemic dataset 
+#load native plant dataset 
 country <- read.csv("intermediate_data/country_dataset.csv")
 
-#rename taxon to match name_use 
-growth_form <- rename(growth_form, 'species' = taxon_name)
+
+#process of joining the two data sets 
+growth_form <- select(growth_form, "species" = taxon_name, value) |> #select only necessary info and rename taxon to species 
+  distinct(species, .keep_all = TRUE) #select only first one of each canonical name 
 
 #join two datasets 
-join_data <- left_join(country, growth_form, by = "species")
+join_data <- left_join(country, growth_form, by = "species") |>
+  select(species, aus_endemic, value) #select only relevant information
 
-#subset so only endemic 
-endemic_subset <- join_data %>%
-  group_by(aus_endemic) %>% 
-  filter(aus_endemic == "TRUE")
 
-#check the right number of observations
-join_data %>% 
-  group_by(aus_endemic) %>% 
-  summarise(aus_endemic=n())
+#creating larger groupings 
+unique(join_data$value) #check what current growth forms the data set has
 
-#endemic treemap data  
-endemic_counts <- endemic_subset %>% 
-  group_by(value) %>% 
-  summarise(num_species=n())
-
-#all endemic values treemap 
-treemap(endemic_counts, 
-        index="value",
-        vSize ="num_species", 
-        type = "index",
-        palette ="BrBG")
-
-#non-endemic tree map 
-#subset so only non-endemic 
-nonendemic_subset <- join_data %>% 
-  group_by(species) %>% 
-  filter(aus_endemic == "FALSE")
-
-#check the right number of observations
-join_data %>% 
-  group_by(aus_endemic) %>% 
-  summarise(aus_endemic=n())
-
-#non-endemic treemap data 
- nonendemic_counts<- nonendemic_subset %>% 
-  group_by(value) %>% 
-  summarise(num_species=n())
-
-#all non-endemic values treemap 
-treemap(nonendemic_counts, 
-        index="value",
-        vSize ="num_species", 
-        type = "index",
-        palette ="BrBG",
-        )
-
-#larger groups 
-unique(join_data$value)
-
-#create larger groupings using recode 
-complete_group <- join_data %>% 
-  group_by(value) %>%
+#create larger groupings using recode (there has got to be a better way of doing this)
+#also need to update what I group as what
+complete_group <- join_data |>
+  group_by(value) |>
   mutate (group = recode(value, 
                          climber_herb = "climber", 
                          climber_liana = "climber",
@@ -130,177 +82,48 @@ unique(complete_group$group)
 #select only necessary info
 complete_group <- select(complete_group, species, value, aus_endemic, group)
 
-#subset so only endemic
-endemic_group <- complete_group %>%
-  group_by(aus_endemic) %>% 
-  filter(aus_endemic == "TRUE")
-
-#endemic treemap of large groups data 
-endemic_group_counts <- endemic_group %>% 
-  group_by(group) %>% 
+#growth form of all natives larger groups
+natives <- complete_group |>
+  group_by(group) |>
   summarise(num_species=n())
 
-treemap(endemic_group_counts,
+#treemap
+treemap(natives,
         index= "group",
         vSize="num_species",
-        type="index", 
-        palette = "Set3") 
+        vColor = "num_species",
+        type="value")
 
-#nonendemic
-#subset so nonendemic
-nonendemic_group <- complete_group %>%
-  group_by(aus_endemic) %>% 
+#ENDEMIC
+endemic_group <- complete_group |>
+  group_by(aus_endemic) |>
+  filter(aus_endemic == "TRUE") 
+
+endemic_group_counts <- endemic_group |>
+  group_by(group) |>
+  summarise(num_species=n())
+
+treemap(endemic_group_counts, 
+        index= "group",
+        vSize="num_species",
+        vColor = "num_species",
+        type="value")
+
+#NONENDEMIC
+nonendemic_group <- complete_group |>
+  group_by(aus_endemic) |> 
   filter(aus_endemic == "FALSE")
 
-#nonendemic treemap of large groups data 
-nonendemic_group_counts <- nonendemic_group %>% 
-  group_by(group) %>% 
+nonendemic_group_counts <- nonendemic_group |>
+  group_by(group) |>
   summarise(num_species=n())
 
 treemap(nonendemic_group_counts,
         index= "group",
         vSize="num_species",
-        type="index", 
-        palette = "Set3") 
+        vColor = "num_species",
+        type="value")
 
 
-#tree map with groups and subgroups 
-#endemic treemap data 
-endemic_subgroup_counts <- complete_group %>% 
-  group_by(value) %>% 
-  summarise(num_species=n())
-
-#join value to groups
-group <- left_join(endemic_group_counts, complete_group, by = "group")
-
-#sub grouping treemap 
-treemap(group,
-        index=c("group","value"),
-        vSize="num_species",
-        type="index", 
-        palette = "BrBG", 
-        title = "Endemic Flora Growth Forms",
-        fontsize.labels=c(15,12),                
-        fontcolor.labels=c("white","grey"),    
-        fontface.labels=c(2,1),                  
-        bg.labels=c("transparent"),            
-        align.labels=list(
-          c("center", "center"), 
-          c("right", "bottom")
-        ),                                   
-        overlap.labels=0,                     
-        inflate.labels=F,
-        border.col=c("black","white"),
-        border.lwds=c(4,1), 
-) 
-
-
-#interactive endemic treemap
-library(tidyverse)
-library(highcharter) 
-options(highcharter.theme = hc_theme_smpl(tooltip = list(valueDecimals = 2)))
-hc <- endemic_group_counts %>%
-  hchart(
-    "treemap", 
-    hcaes(x = group, value = num_species, color = value)
-  )
-hc
-
-endemic_group_counts <- na.omit(endemic_group_counts)
-
-hc <- endemic_group_counts %>%
-  hchart(
-    "treemap", 
-    hcaes(x = group, value = num_species, color = value)
-  )
-hc
-
-#interactive nonendemic treemap
-nonendemic_group_counts <- na.omit(nonendemic_group_counts)
-hc <- nonendemic_group_counts %>%
-  hchart(
-    "treemap", 
-    hcaes(x = group, value = num_species, color = value)
-  )
-hc
-#other attempt
-
-endemic <- group %>%
-  group_by(aus_endemic) %>% 
-  filter(aus_endemic == "TRUE")
-
-hchart(
-  data_to_hierarchical(endemic, c(group, value), num_species),
-  type = "treemap",
-  levelIsConstant = FALSE,
-  allowDrillToNode = TRUE,
-  levels = lvl_opts,
-  tooltip = list(valueDecimals = FALSE)
-) %>% 
-  hc_chart(
-    style = list(fontFamily = "Arial")
-  ) %>% 
-  hc_title(
-    text = "Endemic Growth Forms",
-    style = list(fontFamily = "Arial")
-  ) %>% 
-  hc_size(height = 700)
-
-lvl_opts <-  list(
-  list(
-    level = 1,
-    borderWidth = 0,
-    borderColor = "transparent",
-    dataLabels = list(
-      enabled = TRUE,
-      align = "left",
-      verticalAlign = "top",
-      style = list(
-        fontSize = "12px", 
-        textOutline = FALSE,
-        color = "white",
-        fontWeight = "normal"
-      )
-    )
-  ),
-  list(
-    level = 2,
-    borderWidth = 0,
-    borderColor = "transparent",
-    colorVariation = list(key = "brightness", to = 0.50),
-    dataLabels = list(enabled = FALSE),
-    style = list(
-      fontSize = "10px",
-      textOutline = FALSE, 
-      color = "white", 
-      fontWeight = "normal"
-    )
-  )
-)
-
-
-#interactive nonendemic treemap
-
-nonendemic <- group %>%
-  group_by(aus_endemic) %>% 
-  filter(aus_endemic == "FALSE")
-
-hchart(
-  data_to_hierarchical(nonendemic, c(group, value), num_species),
-  type = "treemap",
-  # levelIsConstant = FALSE,
-  allowDrillToNode = TRUE,
-  levels = lvl_opts,
-  tooltip = list(valueDecimals = TRUE)
-) %>% 
-  hc_chart(
-    style = list(fontFamily = "Arial")
-  ) %>% 
-  hc_title(
-    text = "Nonendemic Growth Forms",
-    style = list(fontFamily = "Arial")
-  ) %>% 
-  hc_size(height = 700)
-
-#stats attempt
+#possible stats??
 chisq.test(complete_group$aus_endemic, complete_group$group)
